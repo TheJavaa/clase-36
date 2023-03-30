0abe922148d3a16f5971af2e740a36f8c7defc65
Java <javaibu98@gmail.com>
2023-03-30 19:16:10 -0300

    se agrega proyecto

---
+++ src/notifications/whatsapp/sendWa.js
@@ -0,0 +1,16 @@
+import twilio from 'twilio'
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+
+const client = twilio(accountSid, authToken);
+
+try{
+    const message = await client.messages.create({
+        body: '¡Nuevo usuario Creado! Se ha creado un nuevo usuario a través de la API. UUID: 62b0f6c1afebb1902faa81e5',
+        from: 'whatsapp',
+        to: 'whatsapp'
+    })
+    console.log(message)
+} catch (error){
+    console.log(error)
+}
